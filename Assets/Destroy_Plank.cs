dfe1573abf3d4d7481e09173e3113ccec8ae425a
Joshua Hom <146480970+Fluffybaggypants@users.noreply.github.com>
2024-10-17 21:26:02 -0400

    Some Stuff

---
+++ Assets/Destroy_Plank.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy_Plank : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void DestroyObject ()
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{   //Work #1
+   // int number = 12;
+    int[] numberArray = new int[5];
+
+    //Work #2
+    string[] stringArray = new string[5];
+
+    //Work #3
+    public GameObject[] Planks;
+
+    public GameObject other;
+    // Start is called before the first frame update
+    void Start()
+    {
+        numberArray[0] = 2;
+        numberArray[1] = 3;
+        numberArray[2] = 4;
+        numberArray[3] = 5;
+        numberArray[4] = 6;
+
+        Planks = GameObject.FindGameObjectsWithTag("Plank");
+
+        for (int i = 0; i < Planks.Length; i++) 
+        {
+            Debug.Log("Plank Number" + i + " is named " + Planks[i].name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (numberArray[0] == 2) 
+        {
+            if (Input.GetKeyDown(KeyCode.W)) 
+            {
+                Debug.Log("Punch");
+              //  Destroy(Planks);
+            }
+        }
+        if (numberArray[1] == 3) 
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) 
+            {
+                Debug.Log("Punch");
+            }
+        }
+        if (numberArray[2] == 4) 
+        {
+            if (Input.GetKeyDown(KeyCode.A)) 
+            {
+                Debug.Log("Punch");
+            }
+        }
+        if (numberArray[3] == 5) 
+        {
+            if (Input.GetKeyDown(KeyCode.D)) 
+            {
+                Debug.Log("Punch");
+            }
+        }
+        if (numberArray[4] == 6) 
+        {
+            if (Input.GetKeyDown(KeyCode.S)) 
+            {
+                Debug.Log("Punch");
+            }
+        }
+    }
+}
